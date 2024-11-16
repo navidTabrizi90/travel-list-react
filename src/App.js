b3bba60d4c87a9eb6d1dfdd98fc7da8829fb88f4
Navid <navidtabrizi90@gmail.com>
2024-11-16 19:00:01 +0100

    adding sort feature

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm("Are you sure to clear the list?");
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -24,6 +29,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats />
     </div>
@@ -73,11 +79,26 @@ function Form({ setItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+  if (sortBy === "description") {
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = [...items].sort((a, b) => a.packed - b.packed);
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -86,6 +107,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={() => onClearList()}>Clear list</button>
+      </div>
     </div>
   );
 }
@@ -99,8 +128,7 @@ function Item({ item, onDeleteItem, onToggleItem }) {
         onChange={() => onToggleItem(item.id)}
       />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity}
-        {item.description}
+        {item.quantity} {item.description}
       </span>
       <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
